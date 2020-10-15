a944d11d716e517178f850f8a5116d32c9d8020e
sriram <sriramchaudhury@gmail.com>
2020-10-15 17:27:50 +0530

    updated largest-np-pp-with-sbar to handle both s and sbar inside a sentence

--- prog/largest-np-pp-with-sbar.py
+++ prog/largest-np-pp-with-sbar.py
@@ -42,15 +42,35 @@ def largest_np_pp(const_tree):
     np_pp_dic = {}
     sbar_dic = {}
     sbar_dic_id = {}
+    s_dic = {}
+    s_dic_id = {}
     for i in range(0, len(w_id)):
         if(w_id[i].strip() != '_'):
             found_sbar = 0
             head_list = get_ancestor1(A_id, H_id, i)
             pos_list = []
-            for j in range(0,len(head_list)):
+
+            #for finding 's'(sentences) inside the main S1
+            for j in range(len(head_list), 0, -1):
+            #for j in range(0,len(head_list)):
                 c_pos = pos_id[head_list[j-1] - 1].strip()
                 pos_list.append(c_pos)
-                if re.search(r'SBAR|S', c_pos) and c_pos != 'S1':
+                if re.search(r'S', c_pos) and c_pos != 'S1':
+                    found_sbar = 1
+                    if (c_pos in s_dic):
+                        s_dic[c_pos] = s_dic[c_pos] + ' ' + w_id[i].strip()
+                        s_dic_id[c_pos] = s_dic_id[c_pos] + ',' + str(i)
+                    else:
+                        s_dic[c_pos] = w_id[i].strip()
+                        s_dic_id[c_pos] = str(head_list[j-1]) + ',' + str(i)
+                    break
+
+            #for finding 'sbar'(sbar sentences) inside the main S1
+            for j in range(len(head_list), 0, -1):
+            #for j in range(0,len(head_list)):
+                c_pos = pos_id[head_list[j-1] - 1].strip()
+                pos_list.append(c_pos)
+                if re.search(r'SBAR', c_pos) and c_pos != 'S1':
                     found_sbar = 1
                     if (c_pos in sbar_dic):
                         sbar_dic[c_pos] = sbar_dic[c_pos] + ' ' + w_id[i].strip()
@@ -59,6 +79,7 @@ def largest_np_pp(const_tree):
                         sbar_dic[c_pos] = w_id[i].strip()
                         sbar_dic_id[c_pos] = str(head_list[j-1]) + ',' + str(i)
                     break
+
 
             if(found_sbar == 0):
                 for j in range(len(head_list),0, -1):
@@ -89,6 +110,26 @@ def largest_np_pp(const_tree):
                            else:
                                np_pp_dic[c_pos] = w_id[int(ids[i])].strip()
                            break
+
+    if(len(s_dic_id) > 0):
+       for key in s_dic_id:
+           ids = s_dic_id[key].split(',')[1:]
+           s_id = s_dic_id[key].split(',')[0]
+
+           print(key, s_dic[key])
+
+           for i in range(0, len(ids)):
+               if (w_id[int(ids[i])].strip() != '_'):
+                   head_list = get_ancestor1(A_id, H_id, int(ids[i]))
+                   for j in range(len(head_list), 0, -1):
+                       c_pos = pos_id[head_list[j - 1] - 1]
+                       if re.search(r'NP|PP', c_pos) and head_list[j-1] < int(s_id):
+                           if (c_pos in np_pp_dic):
+                               np_pp_dic[c_pos] = np_pp_dic[c_pos] + ' ' + w_id[int(ids[i])].strip()
+                           else:
+                               np_pp_dic[c_pos] = w_id[int(ids[i])].strip()
+                           break
+
 
 
     return (np_pp_dic)
